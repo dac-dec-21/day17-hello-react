ab67abd63b5465ba1ca9e692cf27f0fc266a9ac9
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-02 18:44:50 +0530

    b5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// JSX
-// YOU CAN WRITE HTML using JS.
+// Why this variable name is starts with Captial Letter.
+// JSX => Why we are calling it X => XML, not HTML
 const App = () => {
-  return <h1>Hello World</h1>;
+  return <h1>Hello World!</h1>;
 };
 
 export default App;
