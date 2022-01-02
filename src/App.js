60fac260757316f7048af60c6a0dde15f31edb39
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-02 19:25:41 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
+
 export default function App() {
-  let counter = 100;
-  let username = "Hello Rohit";
-  let email = "rohit@gmail.com";
+  let [counter, setCounter] = useState(100);
+
+  const incrment = () => setCounter(counter + 1);
+  const decrement = () => setCounter(counter - 1);
 
   return (
     <div>
       <div> {counter} </div>
-      <div> {username} </div>
-      <div> {email} </div>
+      <input type="button" value="Incrment" onClick={incrment} />
+      <input type="button" value="Decrement" onClick={decrement} />
     </div>
   );
 }
