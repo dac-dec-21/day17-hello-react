9ca9032a6dedf64e239ae69ece219330421cfdbc
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-02 18:34:13 +0530

    b3.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
  * Arrow Function
  * Function Name App
  */
-const App = () => {
+const HelloWorld = () => {
   return "I am the Arrow Function!";
 };
 
-export default App;
+export default HelloWorld;
