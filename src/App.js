bfef49ffa1ca715b0863f01e6464eba9279a536c
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-02 19:19:03 +0530

    b7

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-// JSX
 export default function App() {
+  let counter = 100;
+  let username = "Hello Rohit";
+  let email = "rohit@gmail.com";
+
   return (
     <div>
-      <h1>Hello World</h1>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad officia
-        corporis mollitia quas fugit quibusdam minima similique assumenda labore
-        necessitatibus, architecto aliquid consequatur rem, deserunt quis
-        provident modi, molestias natus.
-      </div>
-
-      <div>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque
-        dicta fuga unde mollitia quo exercitationem assumenda aut vel delectus,
-        blanditiis dolore soluta tenetur quod modi est consequuntur nisi omnis!
-        Doloribus.
-      </div>
+      <div>{counter}</div>
+      <div> {username} </div>
+      <div> {email} </div>
     </div>
   );
 }
