b94784b6aa3124050e93733f55e2003eca87fd49
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-02 18:49:16 +0530

    6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// Why this variable name is starts with Captial Letter.
-// JSX => Why we are calling it X => XML, not HTML
-const App = () => {
-  return <h1>Hello World!</h1>;
-};
-
-export default App;
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
