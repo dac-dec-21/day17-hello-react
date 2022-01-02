4fbfd1c244da80cffdd19f95eb67c0fd5cc7596f
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-02 19:12:44 +0530

    b6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+// JSX
 export default function App() {
   return (
     <div>
-      <h1>Write Hello World USing Atleast Two Tags</h1>
+      <h1>Hello World</h1>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad officia
+        corporis mollitia quas fugit quibusdam minima similique assumenda labore
+        necessitatibus, architecto aliquid consequatur rem, deserunt quis
+        provident modi, molestias natus.
+      </div>
+
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque
+        dicta fuga unde mollitia quo exercitationem assumenda aut vel delectus,
+        blanditiis dolore soluta tenetur quod modi est consequuntur nisi omnis!
+        Doloribus.
+      </div>
     </div>
   );
 }
